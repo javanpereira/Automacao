a5fd3d1334a3f608cfdbd8e06ce120e6aae14a10
javanpereira <42705326+javanpereira@users.noreply.github.com>
2021-03-25 11:03:53 -0300

    Add files via upload

---
+++ Teste/src/javan/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package javan;
+
+/**
+ * @author javan
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Javan");
+
+	}
+
+}
